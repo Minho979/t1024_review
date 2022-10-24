2a4c9ce2bdd5ac1bf034e535f6b0737a3d2886c0
Kim, Minho <minho9779@gmail.com>
2022-10-24 17:22:12 +0900

    Main #3

--- src/t1024/Main.java
+++ src/t1024/Main.java
@@ -3,7 +3,7 @@ package t1024;
 public class Main {
     
     public static void main(String[] args) {
-        String s = "hello world";
+        String s = "안녕하세요";
         System.out.println(s);
     }
 }
